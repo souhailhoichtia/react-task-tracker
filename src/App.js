0ce5efae84176675c2a8a3028e3bf7844107eabe
Souhail Hoichtia <souhailsrp@gmail.com>
2021-11-28 23:02:43 +0100

    adding the view of the form ADD-TASK

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -40,7 +41,8 @@ function App() {
   };
   return (
     <div className="container">
-      <Header title="Task Tracker"></Header>
+      <Header title="Task Tracker" />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
